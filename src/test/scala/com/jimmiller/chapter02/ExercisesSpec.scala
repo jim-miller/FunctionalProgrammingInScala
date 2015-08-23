b9989f72f36cace5b3a848a930e87c5d0ba2bd64
Jim Miller <miller.jimd@gmail.com>
2015-08-22 20:41:44 -0700

    Spelling fix

--- src/test/scala/com/jimmiller/chapter02/ExercisesSpec.scala
+++ src/test/scala/com/jimmiller/chapter02/ExercisesSpec.scala
@@ -3,8 +3,8 @@ package com.jimmiller.chapter02
 import com.jimmiller.chapter02.Exercise2_1._
 import org.scalatest.FlatSpecLike
 
-class ExcercisesSpec extends FlatSpecLike {
-  "Excercise 2.1" should "produce a fibonacci sum for a given integer" in {
+class ExercisesSpec extends FlatSpecLike {
+  "Exercise 2.1" should "produce a fibonacci sum for a given integer" in {
     assert(Exercise2_1.fib(0) === 0)
     assert(Exercise2_1.fib(1) === 1)
     assert(Exercise2_1.fib(5) === 5)
