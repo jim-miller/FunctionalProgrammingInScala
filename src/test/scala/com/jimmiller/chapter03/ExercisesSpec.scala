4153c6e0e16015b518d0c038f96fd11b006bf09b
Miller, Jim (ETW) <jim.miller2@nike.com>
2015-10-10 10:54:30 -0700

    Completed exercise 3.1

--- src/test/scala/com/jimmiller/chapter03/ExercisesSpec.scala
+++ src/test/scala/com/jimmiller/chapter03/ExercisesSpec.scala
@@ -1,17 +1,17 @@
 package com.jimmiller.chapter03
 
-import fpinscala.datastructures.List
-import org.scalatest.FlatSpecLike
+import org.scalatest.{Matchers, FlatSpecLike}
 
-class ExercisesSpec extends FlatSpecLike {
+class ExercisesSpec extends FlatSpecLike with Matchers {
+
   "Exercise 3.1" should "produce the predicted output" in {
-        //  val x = List(1, 2, 3, 4, 5) match {
-        //    case Cons(x, Cons(2, Cons(4, _))) => x
-        //    case Nil => 42
-        //    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-        //    case Cons(h, t) => h + sum(t)
-        //    case _ => 101
-        //  }
-    assert(List.x == 1 + 2)
+    val matchedResult = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+      case _ => 101
+    }
+    matchedResult shouldBe 3
   }
 }
